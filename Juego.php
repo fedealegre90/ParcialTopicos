beec28dc0e5758f4004ac44f98e41edf73f365b8
federico <federico@gmail.com.ar>
2017-11-27 18:02:23 -0300

    test: testCantidadFrame() y testVerificarFrame() - Prueba: Exito

--- Juego.php
+++ Juego.php
@@ -10,11 +10,21 @@ function __construct()
 	public function sumar($frame1){
 
 		$sumaFrame = array_sum($frame1);
-		return $sumaFrame;
+		if ($sumaFrame < 11){
+			return $sumaFrame;
+		}else{
+			echo "\nLa suma de los numeros ingresados no deben superar 10\n";
+		}
+
 	}
 
 	public function verificarFrame($frameX){
+		if (count($frameX) > 2 ){
+			echo "\nLa matriz debe contener solo dos elementos";
+			return [0,0];
+		}else{
 		return $frameX;
+		}
 	}
 
 
