53090ad17d29421f0edd3bd0e2b794e8097212d3
federico <federico@gmail.com.ar>
2017-11-27 22:33:50 -0300

    test: testverificarTotal() - Prueba: Exito

--- Juego.php
+++ Juego.php
@@ -29,6 +29,7 @@ function __construct()
 
 	public function contarPuntaje($frame1,$frame2,$frame3,$frame4,$frame5,$frame6,$frame7,$frame8,$frame9,$frame10){
 		
+		$array=[];
 		$array[0][0]=$frame1[0];
 		$array[0][1]=$frame1[1];
 		$array[1][0]=$frame2[0];
@@ -54,25 +55,32 @@ function __construct()
 		//echo $array[7][1];
 		for ($i=0; $i < 10; $i++) { 
 		//print_r ($array[i][0]); 	
-		if ((($array[$i][0][0]+$array[$i][1][1])==$aux) && (($array[$i+1][0][0]+$array[$i+1][1][1])==$aux)) {
+		if ($i<8){
+		if ((($array[$i][0]+$array[$i][1])==$aux) and (($array[$i+1][0]+$array[$i+1][1])==$aux)) {
 			
 		$sumaFrame=$sumaFrame+$array[$i][0]+$array[$i+1][0]+$array[$i+2][0]+$array[$i+2][1];
 			
-		}elseif ((($array[$i][0][0]+$array[$i][1][1])==$aux) && (($array[$i+1][0][0]+$array[$i+1][1][1])<$aux)) {
+		}elseif ((($array[$i][0]+$array[$i][1])==$aux) and (($array[$i+1][0]+$array[$i+1][1])<$aux)) {
 			$sumaFrame=$sumaFrame+$array[$i][0]+$array[$i+1][0]+$array[$i+1][1];
 		}else{
-			$sumaFrame=$sumaFrame+$array[$i+1][0]+$array[$i+1][1];
+			$sumaFrame=$sumaFrame+$array[$i][0]+$array[$i][1];
 		}	
+		}elseif ($i==8){
+		 	if((($array[$i][0]+$array[$i][1])==$aux) and (($array[$i+1][0]+$array[$i+1][1])<$aux)) {
+			$sumaFrame=$sumaFrame+$array[$i][0]+$array[$i+1][0]+$array[$i+1][1];
+			}else{
+			$sumaFrame=$sumaFrame+$array[$i][0]+$array[$i][1];	
+			}
+		}else{
+			$sumaFrame=$sumaFrame+$array[$i][0]+$array[$i][1];	
+		}
 		}
 		//$suma= array_sum($frame1)+ array_sum($frame2)+ array_sum($frame3)+ array_sum($frame4)+ array_sum($frame5)+ array_sum($frame6)+ array_sum($frame7)+ array_sum($frame8)+array_sum($frame9)+ array_sum($frame10);
-		echo "\n$sumaFrame\n";
-		print_r($array);
+		//echo "\n$sumaFrame\n";
+		//print_r($array);
 		return $sumaFrame;
 		
 	}
-
-
-
 
 }
 
